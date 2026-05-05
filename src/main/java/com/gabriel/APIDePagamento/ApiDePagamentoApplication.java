9399055fd098551eb755e0186001704af2c6a792
gabriel <gabrielfigueira88@gmail.com>
2026-05-04 21:29:58 -0300

    first commit

---
+++ src/main/java/com/gabriel/APIDePagamento/ApiDePagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.APIDePagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDePagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDePagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/APIDePagamento/ApiDePagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.APIDePagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDePagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
